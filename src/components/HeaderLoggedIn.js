d874d0b893271205b58e1b91863e17c1949ed865
connor.tsui <connor.tsui20@gmail.com>
2021-06-18 17:41:20 +0800

    day 4 version 5.1: minor tweak

--- src/components/HeaderLoggedIn.js
+++ src/components/HeaderLoggedIn.js
@@ -7,28 +7,6 @@ import Button from "@material-ui/core/Button";
 import AddIcon from "@material-ui/icons/Add";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 
-import PropTypes from 'prop-types';
-import Typography from '@material-ui/core/Typography';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import Box from '@material-ui/core/Box';
-import Container from '@material-ui/core/Container';
-
-function ElevationScroll(props) {
-    const { children, window } = props;
-    // Note that you normally won't need to set the window ref as useScrollTrigger
-    // will default to window.
-    // This is only being set here because the demo is in an iframe.
-    const trigger = useScrollTrigger({
-      disableHysteresis: true,
-      threshold: 0,
-      target: window ? window() : undefined,
-    });
-  
-    return React.cloneElement(children, {
-      elevation: trigger ? 4 : 0,
-    });
-  }
 
 export default function HeaderLoggedIn({ handleOpenBarcode, handleOpenLogoutCheck }) { 
     //handleOpenBarcode opens barcode scanning page
@@ -38,7 +16,7 @@ export default function HeaderLoggedIn({ handleOpenBarcode, handleOpenLogoutChec
 
         header: { //header color
             backgroundColor: "#039BE6",
-            
+            marginBottom: theme.spacing(5)
         },
 
         button: { //button colors
@@ -57,8 +35,6 @@ export default function HeaderLoggedIn({ handleOpenBarcode, handleOpenLogoutChec
     const headerTheme = useStyles();
 
     return (
-        
-
             <AppBar className={headerTheme.header} position="static">{
                 <Toolbar >
                     <Button className={headerTheme.button} onClick={handleOpenBarcode}>
@@ -70,7 +46,6 @@ export default function HeaderLoggedIn({ handleOpenBarcode, handleOpenLogoutChec
                         </Button></div>
                 </Toolbar>
             }</AppBar> 
-        
     );
 
 }
