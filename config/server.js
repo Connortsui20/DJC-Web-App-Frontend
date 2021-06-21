f1ccca63ff33834dabcadcd48a0ba2b397c12c19
Connor Tsui <connor.tsui20@gmail.com>
2021-06-21 17:24:58 +0800

    Day 5 initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '49df8b078dbfad031a1b5f6d03462274'),
+    },
+  },
+});
