930b7b301943521b2ade34121c2a66c912e4d813
Connor Tsui <connor.tsui20@gmail.com>
2021-06-22 17:50:19 +0800

    day 6 version 3: working data pagination

--- api/barcodescan/controllers/barcodescan.js
+++ api/barcodescan/controllers/barcodescan.js
@@ -1,39 +1,20 @@
 //'use strict';
-//import qs from 'qs';
-//TODO Create custom controller for 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+
 var qs = require('qs');
 
 const { sanitizeEntity } = require('strapi-utils');
 
-//! https://koajs.com/#request
+//! https://koajs.com/#request look at this after everything else is done
+
+//TODO Add custom routes here and in routes.json for create, update, and delete barcodes
 
 module.exports = {
 
-
-
-    /**
-    * Retrieve records.
-    *
-    * @return {Array}
-    */
-
     //! CUSTOM CODE IN HERE: async {custom name}(ctx) { custom code }
-    async custom(ctx) {
+    async userbarcodes(ctx) {
 
-        //ctx.headers['authorization']
-        
-
-        // ctx.state.query = await qs.parse(ctx.querystring)
-
-        //console.log(ctx.query);
-
-        
-        //console.log(ctx.state.query={id:1});
-
+        //const jwt = ctx.header="Authorization";
+      
         let entities;
 
         if (ctx.query._q) {
@@ -43,30 +24,30 @@ module.exports = {
         
         else {
 
+            const userID = ctx.state.user.id; //TODO try and filter out the extra information
 
-            const userID = 1; //TODO get the user id somehow from the api
-            
-            ctx.state.query = qs.parse(ctx.state.querystring=`users_permissions_user.id=${userID}`)
-             //? This outputs a lot of data, even if it is filtered.
-             //? For performance concerns, do I need to filter that out as well?
-            
-            console.log("header", ctx.headers);
+            const start = ctx.query.start;
+            const limit = ctx.query.limit;
 
+            ctx.state.query = qs.parse(ctx.state.querystring=`users_permissions_user.id=${userID}&_start=${start}&_limit=${limit}`);
+            console.log(ctx.query);
+
+            //TODO Look at http://knexjs.org/#Builder
 
             entities = await strapi.services.barcodescan.find(ctx.state.query);
-            //console.log(entities);
-            //console.log("entities", entities);
-            // const newArray = entities.filter(function (barcode) {
-            //     return (barcode.users_permissions_user.id == 2);
-            // });
-            //console.log(newArray);
-        }
+            
 
-        return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.barcodescan }));
+            console.log("%c this should be the header:",  ctx.header)            
+            console.log("%c this should be a user:",  ctx.state.user);
+            //console.log("entities:", entities);
+
+        }
+        
+        const finalArray = entities.map(entity => sanitizeEntity(entity, { model: strapi.models.barcodescan }));
+        //finalArray.push([ctx.header]);
+
+        return (finalArray);
     },
-    // async custom(ctx) {
-    //     return 'Hello World from custom!';
-    //   },
 
 
 };
