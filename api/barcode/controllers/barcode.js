87d34bc464015ed68a9fd619558907612b02d60a
Connor Tsui <connor.tsui20@gmail.com>
2021-06-24 18:36:14 +0800

    day 8 version 1: cleaned up code

--- api/barcode/controllers/barcode.js
+++ api/barcode/controllers/barcode.js
@@ -1,85 +1,54 @@
 //'use strict';
-
 var qs = require('qs');
-
 const { sanitizeEntity } = require('strapi-utils');
 
-//! https://koajs.com/#request look at this after everything else is done
 
-//TODO look into dotenv
-
+//TODO look into dotenv for domain
 module.exports = {
 
     async userbarcodes(ctx) {
 
-        //const jwt = ctx.header="Authorization";
-
         let entities;
-        let newArray; //!change name
+        let finalArray; //!change name
 
         if (ctx.query._q) {
             entities = await strapi.services.barcode.search(ctx.query);
             console.log("ctx.query._q is true");
-        }
-
-        else {
-
-            const userID = ctx.state.user.id; //TODO try and filter out the extra information
-
+        } else {
+            const userID = ctx.state.user.id;
             const start = ctx.query.start;
             const limit = ctx.query.limit;
-
             ctx.state.query = qs.parse(ctx.state.querystring = `users_permissions_user.id=${userID}&_start=${start}&_limit=${limit}`);
-            //console.log(ctx.query);
 
-            //TODO Look at http://knexjs.org/#Builder
-
-            
-            entities = await strapi.services.barcode.find(ctx.state.query);
-
-            //console.log("%c this should be the header:",  ctx.header)            
-            //console.log("%c this should be a user:",  ctx.state.user);
-            //console.log("entities:", entities);
-            newArray = entities.map(item => {
-
+            entities = await strapi.services.barcode.find(ctx.state.query); //TODO Look at http://knexjs.org/#Builder
+            finalArray = entities.map(item => {
                 let newItem = ({
                     ...item
                 });
                 delete newItem.users_permissions_user;
                 return newItem;
-            })
-
+            });
         }
 
-        const finalArray = newArray.map(entity => sanitizeEntity(entity, { model: strapi.models.barcode }));
-        //finalArray.push([ctx.header]);
+        return finalArray.map(entity => sanitizeEntity(entity, { model: strapi.models.barcode }));
 
-
-        //console.log(finalArray);
-        return (finalArray);
     },
 
-    async createbarcode(ctx) { //the same as the create api
+    async createbarcode(ctx) { //the same as the create api, except adding the userID from ctx
 
         let entity;
         const userID = ctx.state.user.id;
-        //console.log(ctx.request.body);
-        if (ctx.is('multipart')) {
+
+        if (ctx.is('multipart')) { //? have no idea what this does, probably not useful in this usecase
             const { data, files } = parseMultipartData(ctx);
             entity = await strapi.services.barcode.create(data, { files });
-
         } else {
-            ctx.request.body['users_permissions_user'] = { id: userID };
+            ctx.request.body['users_permissions_user'] = { id: userID }; //append the userID to the object
             entity = await strapi.services.barcode.create(ctx.request.body);
         }
 
-        const finalBarcode = sanitizeEntity(entity, { model: strapi.models.barcode });
-
-        return finalBarcode;
+        return sanitizeEntity(entity, { model: strapi.models.barcode });
 
     },
-
-
-
 
 };
