533537c76a23c278bed6fe0dfa9f9694bbfccf27
Connor Tsui <connor.tsui20@gmail.com>
2021-06-29 12:35:26 +0800

    day 11 version 2.1: fixed data table style

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 
 import { DataGrid } from "@material-ui/data-grid";
+import { makeStyles } from "@material-ui/core/styles";
 
 import { useTranslation } from "react-i18next";
 
@@ -10,6 +11,15 @@ export default function DataTable({ rows, pageNumber, count, handlePageChange, p
 
     const { t } = useTranslation();
 
+    const useStyles = makeStyles((theme) => ({
+        table: {
+            width: "100%", 
+            marginBottom: theme.spacing(5),
+        }
+    }));
+
+    const dataTheme = useStyles();
+
     const columns = [
         { field: 'id', hide: true },
         { field: "delivery_note_number", headerName: t("Barcode Numbers"), align: "left", headerAlign: "left", width: "50%" },
@@ -17,8 +27,8 @@ export default function DataTable({ rows, pageNumber, count, handlePageChange, p
     ]; //* this only defines and styles the header, column cells are styled in index.css line 1 (.MuiDataGrid-renderingZone !important) to be 100% width
 
     return ( //! How to determine the height?
-        <div style={{ height: 650, width: "100%" }}>
-            <DataGrid rows={rows} columns={columns} page={pageNumber} pageSize={pageSize} rowCount={count} paginationMode={'server'} onPageChange={handlePageChange} disableExtendRowFullWidth={false} />
+        <div className={dataTheme.table}>
+            <DataGrid autoHeight={true} rows={rows} columns={columns} page={pageNumber} pageSize={pageSize} rowCount={count} paginationMode={'server'} onPageChange={handlePageChange} disableExtendRowFullWidth={false} />
         </div>
     );
 
